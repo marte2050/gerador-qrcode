e0ca236d4a1dd238084fc60bb42a638ea755f6cd
marte2050 <150624002+marte2050@users.noreply.github.com>
2023-12-16 19:41:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gerador de QRCODE
+
+Gerador de QRCODE utilizando CSS, JavaScript e a API do Google.
---
+++ js/qrcode.js
@@ -0,0 +1,41 @@
+// Selector do campo input
+const inputData = document.querySelector("#form input")
+// Selector do button, ao qual irá nos permitir escutar um evento
+const buttonEvent = document.querySelector("#form button")
+// Selector do encapsulador do qrcode, que nos permite tornar o campo visível.
+const containerImage = document.querySelector("#qrcode")
+// Campo imagem ao qual receberá os dados advindos da API do google
+const image = document.querySelector("#qrcode img")
+
+buttonEvent.addEventListener("click", () => {
+    const value = getInput();
+    loadingImage(value);
+})
+
+// Obtém os valores do input
+function getInput(){
+    const value = inputData.value;
+    if (!value) return;
+    return value;
+}
+
+function showImage(){
+    containerImage.classList.add('active');
+}
+
+// Faz o reset do campo input e do button
+function resetValueInput(){
+    inputData.value = '';
+    buttonEvent.innerText = 'Gerar novo QRCODE';
+}
+
+// Carrega a imagem gerada pela API do google
+function loadingImage(value){
+    image.src = `https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${value}`;
+    buttonEvent.innerText = 'Aguarde...';
+
+    image.addEventListener('load', ()=>{
+        showImage();
+        resetValueInput();
+    })
+}
